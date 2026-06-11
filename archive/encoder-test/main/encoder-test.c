75dde55fd4230955bf0d0622a19b6fe71a5923fd
ysnd <yusnadi.aunur@gmail.com>
2026-06-11 12:33:22 +0700

    refactor helper read encoder function

--- archive/encoder-test/main/encoder-test.c
+++ archive/encoder-test/main/encoder-test.c
@@ -14,6 +14,13 @@ static const int8_t enc_table[16] = {
     0, +1, -1, 0
 };
 
+typedef struct {
+    uint8_t last_state;
+    int32_t count;
+} encoder_t;
+
+static encoder_t enc = {0};
+
 static const char *TAG = "ENCODER_TEST";
 
 void enc_init(void) {
@@ -25,38 +32,44 @@ void enc_init(void) {
         .intr_type = GPIO_INTR_DISABLE
     };
     gpio_config(&enc_conf);
+    uint8_t a = gpio_get_level(ENC_A);
+    uint8_t b = gpio_get_level(ENC_B);
+
+    enc.last_state = (a << 1) | b;
+}
+
+int8_t encoder_read(void) {
+    uint8_t a = gpio_get_level(ENC_A);
+    uint8_t b = gpio_get_level(ENC_B);
+
+    uint8_t enc_curr = (a << 1) |b;
+
+    if (enc_curr != enc.last_state) {
+        uint8_t idx = (enc.last_state << 2) | enc_curr;
+        enc.count += enc_table[idx];
+        enc.last_state = enc_curr;
+    }
+
+    if (enc.count >= 2) {
+        enc.count -= 2;
+        return 1;
+    }
+
+    if (enc.count <= -2) {
+        enc.count += 2;
+        return -1;
+    }
+    return 0;
 }
 
 void app_main(void) {
     enc_init();
-    uint8_t a = gpio_get_level(ENC_A);
-    uint8_t b = gpio_get_level(ENC_B);
-
-    uint8_t enc_last_state = (a << 1) |b;
-    int32_t enc_count = 0;
     ESP_LOGI(TAG, "ENCODER TESTING READY");
 
     while (1) {
-        a = gpio_get_level(ENC_A);
-        b = gpio_get_level(ENC_B);
-        uint8_t enc_curr = (a << 1) | b;
-
-        if (enc_curr != enc_last_state) {
-            uint8_t idx = (enc_last_state << 2) | enc_curr;
-            enc_count += enc_table[idx];
-            enc_last_state = enc_curr;
-        }
-        //report per 2 counts = 1 detent
-        int8_t wheel = 0;
-        if (enc_count >= 2) {
-            enc_count -= 2; 
-            wheel = 1;  
-            ESP_LOGI(TAG, "SCROLL:%d", wheel);
-        }
-        if (enc_count <= -2) {
-            enc_count += 2;
-            wheel = -1;
-            ESP_LOGI(TAG, "SCROLL:%d", wheel);
+        int8_t wheel = encoder_read();
+        if (wheel != 0) {
+            ESP_LOGI(TAG, "SCROLL: %d", wheel);
         }
         vTaskDelay(1);
     }
